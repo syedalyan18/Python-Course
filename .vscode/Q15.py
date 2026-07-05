ade6f5766bf419275431ec6aee77e05caba29290
0648-Alyan <k250648@nu.edu.pk>
2026-07-05 23:39:03 +0500

    Commit till Q15

--- .vscode/Q15.py
+++ .vscode/Q15.py
@@ -1,1 +1,13 @@
-print("hello world")
+import re
+
+text = input("Enter a text: ")
+
+pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+
+new_text = re.sub(pattern, "[EMAIL REMOVED]", text)
+
+print("\nOriginal Text:")
+print(text)
+
+print("\nModified Text:")
+print(new_text)
